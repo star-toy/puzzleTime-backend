3666d828a6c7dfc6195b6bf349442a73fca7d775
poeekim <hyeonakim0129@gmail.com>
2024-10-29 00:29:14 +0900

    git commit -m "기본 셋팅"

---
+++ src/main/java/startoy/puzzletime/PuzzleTimeApplication.java
@@ -0,0 +1,13 @@
+package startoy.puzzletime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PuzzleTimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PuzzleTimeApplication.class, args);
+	}
+
+}
