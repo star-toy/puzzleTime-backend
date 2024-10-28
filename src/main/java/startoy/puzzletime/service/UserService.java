b9188731b316a3a24b1b71cd2eea9dce5ac67972
poeekim <hyeonakim0129@gmail.com>
2024-10-29 00:50:45 +0900

    Feat: UserService

---
+++ src/main/java/startoy/puzzletime/service/UserService.java
@@ -0,0 +1,18 @@
+package startoy.puzzletime.service;
+
+import startoy.puzzletime.domain.User;
+import startoy.puzzletime.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User findByEmail(String email) {
+        return userRepository.findByEmail(email).orElse(null);
+    }
+}
