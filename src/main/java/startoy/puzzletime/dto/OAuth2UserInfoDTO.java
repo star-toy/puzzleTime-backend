4b11244af0640f6acba456725b61ac87a19f9e5d
poeekim <hyeonakim0129@gmail.com>
2024-11-08 01:25:30 +0900

    Feat: OAuth2UserInfoDto - Notnull 어노테이션 추가

--- src/main/java/startoy/puzzletime/dto/OAuth2UserInfoDTO.java
+++ src/main/java/startoy/puzzletime/dto/OAuth2UserInfoDTO.java
@@ -1,14 +1,19 @@
 package startoy.puzzletime.dto;
 
 //OAuth2 사용자 정보 DTO. 추후 사용자가 필요에 따라 확장할 수 있습니다.
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
-public class OAuth2UserInfoDto {
+public class OAuth2UserInfoDTO {
+    @NotNull
     private String name;
+    @NotNull
     private String email;
+    @NotNull
     private String provider;
+    @NotNull
     private String providerId;
 }
