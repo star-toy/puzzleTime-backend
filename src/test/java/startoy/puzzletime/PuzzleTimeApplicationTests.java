01d4b4de85ace115633153d184a77cf49673b5dc
poeekim <107227456+poeekim@users.noreply.github.com>
2024-11-08 11:18:40 +0900

    Update PuzzleTimeApplicationTests.java
    test 코드 주석 처리

--- src/test/java/startoy/puzzletime/PuzzleTimeApplicationTests.java
+++ src/test/java/startoy/puzzletime/PuzzleTimeApplicationTests.java
@@ -1,8 +1,8 @@
-package startoy.puzzletime;
+// package startoy.puzzletime;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+// import org.junit.jupiter.api.Test;
+// import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class PuzzleTimeApplicationTests {
-}
+// @SpringBootTest
+// class PuzzleTimeApplicationTests {
+// }
