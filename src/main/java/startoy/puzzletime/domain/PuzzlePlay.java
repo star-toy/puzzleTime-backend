081d5bbfd55ec32820400e908c81b1d04842f534
poeekim <hyeonakim0129@gmail.com>
2024-11-11 00:25:21 +0900

    Feat: PuzzlePlay 엔티티 Notnull 어노테이션 추가 및 columnDefinition 수정

--- src/main/java/startoy/puzzletime/domain/PuzzlePlay.java
+++ src/main/java/startoy/puzzletime/domain/PuzzlePlay.java
@@ -1,6 +1,7 @@
 package startoy.puzzletime.domain;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 import java.time.LocalDateTime;
@@ -13,35 +14,40 @@ import java.time.LocalDateTime;
 @Entity
 @Table(name = "tb_puzzle_play")
 public class PuzzlePlay {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "puzzle_play_id")
+    @Column(name = "puzzle_play_id", nullable = false)
+    @NotNull
     private Long puzzlePlayId;
 
+    @NotNull
     @ManyToOne
     @JoinColumn(name = "user_id", referencedColumnName = "user_id", nullable = false)
     private User user;
 
+    @NotNull
     @ManyToOne
     @JoinColumn(name = "puzzle_id", referencedColumnName = "puzzle_id", nullable = false)
     private Puzzle puzzle;
 
+    @NotNull
     @Column(name = "is_completed")
     private Boolean isCompleted;
 
-    @Column(name = "user_stage_pref_id")
+    @NotNull
+    @Column(name = "user_stage_pref_id", nullable = false)
     private Long userStagePrefId;
 
-    @Column(name = "image_id")
+    @NotNull
+    @Column(name = "image_id", nullable = false)
     private Long imageId;
 
-    @Column(name = "pieces_count")
-    private int piecesCount;
-
-    @Column(name = "pieces_data")
+    @Column(name = "pieces_data", columnDefinition = "json")
     private String piecesData;
 
-    @Column(name = "created_at")
+    @NotNull
+    @Column(name = "created_at", nullable = false)
     private LocalDateTime createdAt;
 
     @Column(name = "updated_at")
