ecdff5340d23770069a528ffbc2a7e0dd2db9a35
poeekim <hyeonakim0129@gmail.com>
2024-11-11 17:59:47 +0900

    feat: ErrorResponseDTO - 에러 응답 표준화를 위한 ErrorResponseDTO 추가 (에러 메시지, HTTP 상태 코드, 발생 시간 필드를 포함한 DTO 정의)

--- src/main/java/startoy/puzzletime/exception/ApiExceptionHandler.java
+++ src/main/java/startoy/puzzletime/exception/ApiExceptionHandler.java
@@ -3,7 +3,7 @@ package startoy.puzzletime.exception;
 // 예외 처리 핸들러
 
 import org.springframework.http.HttpStatus;
-import startoy.puzzletime.dto.ErrorResponse;
+import startoy.puzzletime.dto.ErrorResponseDTO;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -15,21 +15,21 @@ public class ApiExceptionHandler {
 
     // CustomException을 처리하는 핸들러 메서드
     @ExceptionHandler(CustomException.class)
-    public ResponseEntity<ErrorResponse> handleCustomException(CustomException ex) {
+    public ResponseEntity<ErrorResponseDTO> handleCustomException(CustomException ex) {
         ErrorCode errorCode = ex.getErrorCode();
 
         // ErrorResponse 생성 및 상태 코드와 함께 반환
-        ErrorResponse errorResponse = ErrorResponseFactory.createErrorResponse(
+        ErrorResponseDTO errorResponseDTO = ErrorResponseFactory.createErrorResponse(
                 errorCode.getMessage(),
                 errorCode.getHttpStatus().value()
         );
-        return new ResponseEntity<>(errorResponse, errorCode.getHttpStatus());
+        return new ResponseEntity<>(errorResponseDTO, errorCode.getHttpStatus());
     }
 
     // 그 외 예상치 못한 예외 처리
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<ErrorResponse> handleException(Exception ex) {
-        ErrorResponse errorResponse = ErrorResponseFactory.createErrorResponse("Internal Server Error", 500);
-        return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+    public ResponseEntity<ErrorResponseDTO> handleException(Exception ex) {
+        ErrorResponseDTO errorResponseDTO = ErrorResponseFactory.createErrorResponse("Internal Server Error", 500);
+        return new ResponseEntity<>(errorResponseDTO, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
