7d6cee362efda59b3cb43253d3efc70e8cf47138
poeekim <hyeonakim0129@gmail.com>
2024-11-11 18:05:29 +0900

    feat: ArtworkDto - Artwork 데이터 전송을 위한 ArtworkDto 추가(ArtworkUid, title, description 필드를 포함하여 Artwork 정보를 담는 DTO 클래스 생성)

---
+++ src/main/java/startoy/puzzletime/dto/ArtworkDto.java
@@ -0,0 +1,17 @@
+package startoy.puzzletime.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ArtworkDto {
+    private String artworkUid;
+    private String artworkTitle;
+    private String artworkDescription;
+    //private int totalStages;
+}
