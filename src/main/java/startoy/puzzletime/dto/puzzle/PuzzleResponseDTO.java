7d02c1ea619b251c030a7a03418b5099d2cc6452
poeekim <hyeonakim0129@gmail.com>
2024-11-14 00:50:00 +0900

    feat: PuzzleResponseDTO -  isCompleted 컬럼 추가

--- src/main/java/startoy/puzzletime/dto/puzzle/PuzzleResponseDTO.java
+++ src/main/java/startoy/puzzletime/dto/puzzle/PuzzleResponseDTO.java
@@ -1,5 +1,6 @@
 package startoy.puzzletime.dto.puzzle;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,5 +15,7 @@ public class PuzzleResponseDTO {
     private int puzzleIndex;       // 퍼즐의 순서 (인덱스)
     private Long puzzleImageId;    // 퍼즐 이미지의 ID
     private String imageUrl;       // 퍼즐 이미지의 URL
-    //private boolean isCompleted; // 완료 여부를 포함
+
+    @JsonProperty("isCompleted")   // JSON에서 필드 이름을 "isCompleted"로 설정 (미적용시 json 응답에 필드명이 completed 로 바뀜)
+    private boolean isCompleted;    // 퍼즐 완료 여부
 }
