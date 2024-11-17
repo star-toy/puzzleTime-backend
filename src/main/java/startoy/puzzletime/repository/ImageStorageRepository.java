2f305c0500be2bb0abdac9b497b19993c8f4ec75
poeekim <hyeonakim0129@gmail.com>
2024-11-17 21:22:32 +0900

    feat: ImageStorageRepository - imageId로 ImageStorage 조회하는 메서드 추가

--- src/main/java/startoy/puzzletime/repository/ImageStorageRepository.java
+++ src/main/java/startoy/puzzletime/repository/ImageStorageRepository.java
@@ -3,8 +3,11 @@ package startoy.puzzletime.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import startoy.puzzletime.domain.ImageStorage;
+import java.util.Optional;
+
 @Repository
 public interface ImageStorageRepository extends JpaRepository<ImageStorage, Long> {
-    // 추가 메서드가 필요하지 않지만, 필요시 정의 가능
+    // imageId로 ImageStorage 조회
+    Optional<ImageStorage> findById(Long imageId);
 
 }
