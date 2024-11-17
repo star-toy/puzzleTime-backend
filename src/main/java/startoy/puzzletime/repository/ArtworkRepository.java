a26ce99d3ac1ea2fdd3cfdbca7187a97de58079f
poeekim <hyeonakim0129@gmail.com>
2024-11-17 21:20:40 +0900

    feat: ArtworkRepository 특정 ThemeId로 Artwork 리스트 조회하는 메서드 추가

--- src/main/java/startoy/puzzletime/repository/ArtworkRepository.java
+++ src/main/java/startoy/puzzletime/repository/ArtworkRepository.java
@@ -3,10 +3,15 @@ package startoy.puzzletime.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import startoy.puzzletime.domain.Artwork;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ArtworkRepository extends JpaRepository<Artwork, Long> {
 
-    Optional<Artwork> findByArtworkUid(String artworkUid); // UID 로 Artwork 조회
+    // UID 로 Artwork 조회
+    Optional<Artwork> findByArtworkUid(String artworkUid);
+
+    // 특정 ThemeId로 Artwork 리스트 조회
+    List<Artwork> findByThemeThemeId(Long themeId);
 
 }
