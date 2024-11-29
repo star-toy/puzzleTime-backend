95a5b59db7cf993bd17fd4bfec9e51fd3eb83e42
poeekim <hyeonakim0129@gmail.com>
2024-11-30 00:10:58 +0900

    Feat: ArtworkController - Authorization parameter 삭제 및 수정

--- src/main/java/startoy/puzzletime/controller/ArtworkController.java
+++ src/main/java/startoy/puzzletime/controller/ArtworkController.java
@@ -21,16 +21,13 @@ public class ArtworkController {
 
     private static final Logger logger = LoggerFactory.getLogger(ArtworkController.class);
     private final ArtworkService artworkService;
-    private final UserService userService;
 
-    // 특정 아트웍의 기본 정보(artworkUid,artworkTitle,artworkDescription) 조회
+    // 특정 아트웍의 기본 정보(artworkUid, artworkTitle, artworkDescription) 조회
     @GetMapping("/{artworkUid}")
     public ResponseEntity<ArtworkDTO> getArtworkByUid(@PathVariable String artworkUid) {
-
         logger.info("아트웍 UID '{}'에 대한 기본 정보 조회 요청을 받았습니다.", artworkUid);
         ArtworkDTO artworkDto = artworkService.getArtworkByUid(artworkUid);
 
-        //  artworkDto가 null이면 NotFoundException 예외 발생
         if (artworkDto == null) {
             logger.warn("아트웍 UID '{}'에 대한 정보가 없습니다.", artworkUid);
             throw new CustomException(ErrorCode.ARTWORK_NOT_FOUND);
@@ -39,23 +36,11 @@ public class ArtworkController {
         return ResponseEntity.ok(artworkDto);
     }
 
-// 특정 아트웍의 기본 정보와 퍼즐 목록을 함께 조회
+    // 특정 아트웍의 기본 정보와 퍼즐 목록을 함께 조회
     @GetMapping("/{artworkUid}/puzzles")
     public ResponseEntity<ArtworkWithPuzzlesResponseDTO> getArtworkWithPuzzles(
             @PathVariable String artworkUid,
-            @RequestHeader(value = "Authorization", required = false) String authorizationHeader) { // AccessToken 값
-
-        String userEmail = null;
-
-        // Access Token 검증 및 이메일 추출
-        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
-            String token = authorizationHeader.substring(7); // "Bearer " 이후의 토큰 값
-            userEmail = userService.getEmailFromAccessToken(token); // Access Token에서 이메일 추출
-
-            if (userEmail == null) {
-                return ResponseEntity.status(401).body(null); // 유효하지 않은 토큰
-            }
-        }
+            @RequestParam(value = "email", required = false) String userEmail) { // 프론트엔드에서 전달받은 사용자 이메일
 
         logger.info("아트웍 UID '{}'의 기본 정보 및 퍼즐 목록 조회 요청을 받았습니다.", artworkUid);
         ArtworkWithPuzzlesResponseDTO responseDto = artworkService.getArtworkWithPuzzlesByUidAndUser(artworkUid, userEmail);
