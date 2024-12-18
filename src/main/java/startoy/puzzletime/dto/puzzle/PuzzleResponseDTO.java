3a37ac7ac975a9d60a9a618ad450c859e02a39aa
poeekim <hyeonakim0129@gmail.com>
2024-12-18 18:07:56 +0900

    Feat: PuzzleResponseDTO - 필드명 통일을 위한 수정

--- src/main/java/startoy/puzzletime/dto/puzzle/PuzzleResponseDTO.java
+++ src/main/java/startoy/puzzletime/dto/puzzle/PuzzleResponseDTO.java
@@ -14,7 +14,7 @@ public class PuzzleResponseDTO {
     private String puzzleUid;      // 퍼즐의 UID
     private int puzzleIndex;       // 퍼즐의 순서 (인덱스)
     private Long puzzleImageId;    // 퍼즐 이미지의 ID
-    private String imageUrl;       // 퍼즐 이미지의 URL
+    private String ImageUrl;       // 퍼즐 이미지의 URL
 
     @JsonProperty("isCompleted")   // JSON에서 필드 이름을 "isCompleted"로 설정 (미적용시 json 응답에 필드명이 completed 로 바뀜)
     private boolean isCompleted;    // 퍼즐 완료 여부
