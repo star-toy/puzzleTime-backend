a21980851e8539b53e3e30878889047565f7e798
poeekim <hyeonakim0129@gmail.com>
2024-12-20 21:25:23 +0900

    Feat: PuzzlePlayController - 기존 myPage 관련 api를  puzzlePlay 로 변경

--- src/main/java/startoy/puzzletime/controller/PuzzlePlayController.java
+++ src/main/java/startoy/puzzletime/controller/PuzzlePlayController.java
@@ -4,7 +4,10 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
+import startoy.puzzletime.dto.puzzlePlay.PlayingPuzzlesResponseDTO;
 import startoy.puzzletime.dto.puzzlePlay.PuzzlePlayRequest;
 import startoy.puzzletime.dto.puzzlePlay.PuzzlePlayResponse;
 import startoy.puzzletime.service.PuzzlePlayService;
@@ -12,7 +15,6 @@ import startoy.puzzletime.service.TokenService;
 import startoy.puzzletime.service.UserService;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 
 @RestController
 @RequiredArgsConstructor
@@ -20,6 +22,7 @@ import org.springframework.security.oauth2.client.authentication.OAuth2Authentic
 @Tag(name = "PuzzlePlays", description = "PuzzlePlays API")
 public class PuzzlePlayController {
 
+    private static final Logger logger = LoggerFactory.getLogger(PuzzlePlayController.class);
     private final PuzzlePlayService puzzlePlayService;
     private final UserService userService;
     private final TokenService tokenService;
@@ -36,6 +39,21 @@ public class PuzzlePlayController {
         Long userId = userService.getUserIdByEmail(email);
 
         PuzzlePlayResponse response = puzzlePlayService.savePuzzlePlay(puzzlePlayUID, userId, request);
+        return ResponseEntity.ok(response);
+    }
+
+    @Operation(summary = "플레이 중인 퍼즐 목록 조회", description = "현재 사용자의 진행 중인 퍼즐 목록을 조회합니다.")
+    @GetMapping("/playing")
+    public ResponseEntity<PlayingPuzzlesResponseDTO> getPlayingPuzzles(@CookieValue(name = "token") String token) {
+
+        // 토큰에서 이메일 추출 후 사용자 ID 조회
+        String email = tokenService.getEmailFromToken(token);
+
+        logger.debug("userEmail : {}", email);
+
+        // 진행 중인 퍼즐 데이터 가져오기
+        PlayingPuzzlesResponseDTO response = puzzlePlayService.getPlayingPuzzles(email);
+
         return ResponseEntity.ok(response);
     }
 }
