d9bd93ffa3dbbc98e798d04e75dc6b86ec835331
poeekim <hyeonakim0129@gmail.com>
2024-12-20 21:23:58 +0900

    Chore: MyPageController - 사용하지 않는 api 삭제를 위한 주석 처리

--- src/main/java/startoy/puzzletime/controller/MyPageController.java
+++ src/main/java/startoy/puzzletime/controller/MyPageController.java
@@ -1,18 +1,10 @@
 package startoy.puzzletime.controller;
 
-import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import startoy.puzzletime.dto.myPage.MyPagePlayingPuzzlesResponseDTO;
-import startoy.puzzletime.service.MyPageService;
 
 @Slf4j
 @RestController
@@ -21,7 +13,7 @@ import startoy.puzzletime.service.MyPageService;
 @Tag(name = "MyPage", description = "MyPage API")
 public class MyPageController {
 
-    private static final Logger logger = LoggerFactory.getLogger(MyPageController.class);
+    /*private static final Logger logger = LoggerFactory.getLogger(MyPageController.class);
     private final MyPageService myPageService;
 
     @Operation(summary = "플레이 중인 퍼즐 목록 조회", description = "현재 사용자의 진행 중인 퍼즐 목록을 조회합니다.")
@@ -37,5 +29,5 @@ public class MyPageController {
         MyPagePlayingPuzzlesResponseDTO response = myPageService.getPlayingPuzzles(userEmail);
 
         return ResponseEntity.ok(response);
-    }
+    }*/
 }
