fae4a8a57c8dc0b2c60135284a662775e55c6b46
poeekim <hyeonakim0129@gmail.com>
2024-12-24 20:54:30 +0900

    Feat: ArtworkController - getArtworkByUid 주석처리 및 getArtworkWithPuzzles 메서드 파라미터 수정

--- src/main/java/startoy/puzzletime/controller/ArtworkController.java
+++ src/main/java/startoy/puzzletime/controller/ArtworkController.java
@@ -1,7 +1,6 @@
 package startoy.puzzletime.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -9,14 +8,11 @@ import org.springframework.web.bind.annotation.*;
 import startoy.puzzletime.dto.artwork.ArtworkDTO;
 import startoy.puzzletime.dto.puzzle.ArtworkWithPuzzlesResponseDTO;
 import startoy.puzzletime.dto.artwork.CompleteArtworksResponse;
-import startoy.puzzletime.dto.puzzlePlay.PuzzlePlayRequest;
-import startoy.puzzletime.dto.puzzlePlay.PuzzlePlayResponse;
 import startoy.puzzletime.exception.CustomException;
 import startoy.puzzletime.exception.ErrorCode;
 import startoy.puzzletime.service.ArtworkService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import startoy.puzzletime.service.PuzzleService;
 import startoy.puzzletime.service.TokenService;
 import startoy.puzzletime.service.UserService;
 
@@ -34,7 +30,7 @@ public class ArtworkController {
     private final TokenService tokenService;
 
     // 특정 아트웍의 기본 정보(artworkUid, artworkTitle, artworkDescription) 조회
-    @GetMapping("/{artworkUid}")
+    /*@GetMapping("/{artworkUid}")
     public ResponseEntity<ArtworkDTO> getArtworkByUid(@PathVariable String artworkUid) {
         logger.info("아트웍 UID '{}'에 대한 기본 정보 조회 요청을 받았습니다.", artworkUid);
         ArtworkDTO artworkDto = artworkService.getArtworkByUid(artworkUid);
@@ -45,7 +41,7 @@ public class ArtworkController {
         }
 
         return ResponseEntity.ok(artworkDto);
-    }
+    }*/
 
     // Mypage - gallery 완성한 artwork과 reward 조회
     @Operation(summary = "complete artworks", description = "갤러리에서 완성한 작품과 보상을 조회")
@@ -65,20 +61,26 @@ public class ArtworkController {
     @GetMapping("/{artworkUid}/puzzles")
     public ResponseEntity<ArtworkWithPuzzlesResponseDTO> getArtworkWithPuzzles(
             @PathVariable String artworkUid,
-            @RequestParam(value = "email", required = false) String userEmail) { // 프론트엔드에서 전달받은 사용자 이메일
+            @CookieValue(name = "token") String token) {
 
-        logger.info("아트웍 UID '{}'의 기본 정보 및 퍼즐 목록 조회 요청을 받았습니다.", artworkUid);
-        ArtworkWithPuzzlesResponseDTO responseDto = artworkService.getArtworkWithPuzzlesByUidAndUser(artworkUid, userEmail);
+            // 토큰에서 이메일 추출
+            String userEmail = tokenService.getEmailFromToken(token);
 
-        if (responseDto == null) {
-            logger.warn("아트웍 UID '{}'에 대한 퍼즐 목록이 없습니다.", artworkUid);
-            throw new CustomException(ErrorCode.PUZZLE_NOT_FOUND);
+            logger.info("계정 '{}'의 아트웍 UID '{}' 기본 정보 및 퍼즐 목록 조회 요청을 받았습니다.", userEmail, artworkUid);
+
+            ArtworkWithPuzzlesResponseDTO responseDto = artworkService.getArtworkWithPuzzlesByUidAndUser(artworkUid, userEmail);
+
+            if (responseDto == null) {
+                logger.warn("아트웍 UID '{}'에 대한 퍼즐 목록이 없습니다.", artworkUid);
+                throw new CustomException(ErrorCode.PUZZLE_NOT_FOUND);
+            }
+
+            // 응답 DTO에 email 설정
+            responseDto.setEmail(userEmail); // 비회원일 경우 null이 설정됨
+
+            return ResponseEntity.ok(responseDto);
         }
 
-        // 응답 DTO에 email 설정
-        responseDto.setEmail(userEmail); // 비회원일 경우 null이 설정됨
-
-        return ResponseEntity.ok(responseDto);
-    }
 }
+
 
