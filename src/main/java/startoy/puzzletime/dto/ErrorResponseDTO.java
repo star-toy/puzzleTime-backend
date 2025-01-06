50890047a2843fbdc9032b6d59cddd81ac954144
poeekim <hyeonakim0129@gmail.com>
2025-01-06 16:28:59 +0900

    Feat: ErrorResponseDTO - 에러 내 CODE 추가

--- src/main/java/startoy/puzzletime/dto/ErrorResponseDTO.java
+++ src/main/java/startoy/puzzletime/dto/ErrorResponseDTO.java
@@ -8,6 +8,7 @@ import java.time.LocalDateTime;
 @Getter
 @AllArgsConstructor
 public class ErrorResponseDTO {
+    private final String code;    // 고유 에러 코드
     private final String message;        // 에러 메시지
     private final int status;            // HTTP 상태 코드
     private final LocalDateTime timestamp;  // 에러 발생 시간
