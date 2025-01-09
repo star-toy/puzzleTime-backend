31dee317d23da89c851b41cd5d4b7a0b3d7d9088
poeekim <hyeonakim0129@gmail.com>
2025-01-09 17:53:00 +0900

    Feat : TOKEN_REISSUE_REQUIRED 추가

--- src/main/java/startoy/puzzletime/controller/PuzzlePlayController.java
+++ src/main/java/startoy/puzzletime/controller/PuzzlePlayController.java
@@ -84,7 +84,7 @@ public class PuzzlePlayController {
                     logger.warn("만료된 토큰으로 요청되었습니다.");
                 } else {
                     logger.warn("유효하지 않은 토큰입니다. 새로 로그인하여 토큰을 발급받으세요.");
-                    throw new CustomException(ErrorCode.INVALID_TOKEN);
+                    throw new CustomException(ErrorCode.TOKEN_REISSUE_REQUIRED);
                 }
             }
         }
--- src/main/java/startoy/puzzletime/exception/ErrorCode.java
+++ src/main/java/startoy/puzzletime/exception/ErrorCode.java
@@ -23,6 +23,7 @@ public enum ErrorCode {
 
     // 토큰 관련 에러
     INVALID_TOKEN("ERR_INVALID_TOKEN", HttpStatus.UNAUTHORIZED, "No token found in cookie"),
+    TOKEN_REISSUE_REQUIRED("ERR_TOKEN_REISSUE_REQUIRED", HttpStatus.UNAUTHORIZED, "The token is Invalid. Please reauthenticate to obtain a new token."),
     TOKEN_EXPIRED("ERR_TOKEN_EXPIRED", HttpStatus.UNAUTHORIZED, "Token has expired"),
     TOKEN_NOT_FOUND("ERR_TOKEN_NOT_FOUND", HttpStatus.UNAUTHORIZED, "Token not found"),
 
